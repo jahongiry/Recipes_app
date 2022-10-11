a0ea2d32a2b75e496dd8547f63490345ebc06397
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-10-11 21:41:35 +0300

    Fix linters and add validation for food name

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,5 @@
 class FoodsController < ApplicationController
-  before_action :set_food, only: %i[ show edit update destroy ]
+  before_action :set_food, only: %i[show edit update destroy]
 
   def index
     @foods = Food.all
@@ -9,15 +9,11 @@ class FoodsController < ApplicationController
     @food = Food.find(params[:id])
   end
 
- 
   def new
     @food = Food.new
   end
 
-
-  def edit
-  end
-
+  def edit; end
 
   def create
     @food = Food.new(food_params)
@@ -25,7 +21,7 @@ class FoodsController < ApplicationController
 
     respond_to do |format|
       if @food.save
-        format.html { redirect_to foods_url, notice: "Food was successfully created." }
+        format.html { redirect_to foods_url, notice: 'Food was successfully created.' }
         format.json { render :show, status: :created, location: @food }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -34,11 +30,10 @@ class FoodsController < ApplicationController
     end
   end
 
- 
   def update
     respond_to do |format|
       if @food.update(food_params)
-        format.html { redirect_to foods_url(@food), notice: "Food was successfully updated." }
+        format.html { redirect_to foods_url(@food), notice: 'Food was successfully updated.' }
         format.json { render :show, status: :ok, location: @food }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -47,24 +42,22 @@ class FoodsController < ApplicationController
     end
   end
 
-  
   def destroy
     @food.destroy
 
     respond_to do |format|
-      format.html { redirect_to foods_url, notice: "Food was successfully destroyed." }
+      format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
 
-    def set_food
-      @food = Food.find(params[:id])
-    end
+  def set_food
+    @food = Food.find(params[:id])
+  end
 
-   
-    def food_params
-      params.require(:food).permit(:name, :measurement_unit, :price, :user_id)
-    end
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :user_id)
+  end
 end
