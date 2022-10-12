9c208fc427c140c3b29ce695ec569b88cf0be252
Zelalem1222 <zelalemfissha394@gmail.com>
2022-10-12 10:19:31 +0300

    Fix linter errors

--- app/models/inventori.rb
+++ app/models/inventori.rb
@@ -1,4 +1,4 @@
 class Inventori < ApplicationRecord
   belongs_to :user
-  has_many :inventory_foods, dependent: :destroy , foreign_key: 'inventory_id'
+  has_many :inventory_foods, dependent: :destroy, foreign_key: 'inventory_id'
 end
--- app/models/inventory_food.rb
+++ app/models/inventory_food.rb
@@ -1,4 +1,4 @@
-class InventoryFood < ApplicationRecord 
-  belongs_to :food , foreign_key: 'food_id'
-  belongs_to :inventori , foreign_key: 'inventory_id'
+class InventoryFood < ApplicationRecord
+  belongs_to :food, foreign_key: 'food_id'
+  belongs_to :inventori, foreign_key: 'inventory_id'
 end
