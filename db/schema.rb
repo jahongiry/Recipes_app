ca4e4b004bed33678f061bfb06fa28dc38085a1e
Zelalem1222 <zelalemfissha394@gmail.com>
2022-10-12 09:02:35 +0300

    Modify inventorie mvc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_134445) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_12_043918) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_134445) do
     t.index ["user_id"], name: "index_inventoris_on_user_id"
   end
 
+  create_table "inventory_foods", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "food_id", null: false
+    t.bigint "inventory_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["food_id"], name: "index_inventory_foods_on_food_id"
+    t.index ["inventory_id"], name: "index_inventory_foods_on_inventory_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -52,4 +62,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_134445) do
 
   add_foreign_key "foods", "users"
   add_foreign_key "inventoris", "users"
+  add_foreign_key "inventory_foods", "foods"
+  add_foreign_key "inventory_foods", "inventoris", column: "inventory_id"
 end
