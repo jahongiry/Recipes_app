2c3304fcdc7d46ce03acf8dcd97c8c65f57db165
Zelalem1222 <zelalemfissha394@gmail.com>
2022-10-12 08:59:23 +0300

    create inventory food migration file

---
+++ db/migrate/20221012043918_inventory_food.rb
@@ -0,0 +1,11 @@
+class InventoryFood < ActiveRecord::Migration[7.0]
+  def change
+    create_table :inventory_foods do |t|
+      t.integer :quantity
+      t.references :food, null: false, foreign_key: { to_table: :foods }
+      t.references :inventory, null: false, foreign_key: { to_table: :inventoris }
+
+      t.timestamps
+    end
+  end
+end
